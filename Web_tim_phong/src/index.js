e72c940e5d56104bac53dcb61e52035e7d4bddce
Nguyen Hung Phi <hungphi2206@gmail.com>
2024-03-14 23:50:31 +0700

    ok

--- Web_tim_phong/src/index.js
+++ Web_tim_phong/src/index.js
@@ -5,9 +5,19 @@ const handlebars = require('express-handlebars')
 const app = express()
 const post = 3000
 
+app.use(express.static(path.join(__dirname, 'public')))
+
 //HTTP logger
 app.use(morgan('combined'))
 
 //Template engine
 app.set('view engine', 'handlebars')
 app.set('views', path.join(__dirname, 'resources/views'))
+
+app.get('/',(req,res) => {
+    res.send("Hello")
+})
+
+app.listen(3000, () => {
+    console.log('Máy chủ đang lắng nghe trên cổng 3000');
+  })
